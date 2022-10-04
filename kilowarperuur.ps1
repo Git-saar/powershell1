4f95e58989352253c9ecd21bb5e108d8bf8e15cb
git_saar@proton.me <SShtiler.didacticum.nl>
2022-10-04 15:14:58 +0200

    ADDING A FUNCTION

--- kilowarperuur.ps1
+++ kilowarperuur.ps1
@@ -1,5 +1,8 @@
 # kilowatt per uur
 
+
+
+
 $KiloWattPerUur = 0.88
 
 $Kilowatt = 1000
@@ -11,6 +14,20 @@ $user_machine = read-host
 
 $PRICE_per_hour = $user_machine/$Kilowatt*$KiloWattPerUur
 
-"Using this machine will cost you " + $PRICE_per_hour  + " cent per hour"
+"Using this machine will cost you " + $PRICE_per_hour  + " euro per hour"
 
 
+function calculator 
+{
+    param($watt)
+
+    $total = ($watt / 1000) * 0.88
+
+    write-host 'Using this machine will cost you'  $total  'huro per hour'
+
+
+
+}
+
+
+calculator -watt 1000000
